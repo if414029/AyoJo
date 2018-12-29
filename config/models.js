83f1536bc480220c63a20fa622f5a5f10e441845
Amos Sitorus <amos@qlue.id>
2018-12-29 15:37:12 +0700

    Config and Models

--- config/models.js
+++ config/models.js
@@ -3,15 +3,12 @@ const config = require('./config')[process.env.NODE_ENV || 'development']
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 const models = {
-    User: sequelize.import('../app/user/user_model'),
-    Blog: sequelize.import('../app/blog/blog_model'),
-    Proyek: sequelize.import('../app/proyek/proyek_model'),
-    Home: sequelize.import('../app/home/home_model'),
-    BlogPicture: sequelize.import('../app/blog/blog_picture_model'),
-    BlogVideo: sequelize.import('../app/blog/blog_video_model'),
-    CategoryBlog: sequelize.import('../app/category_blog/category_blog_model'),
-    Testimony: sequelize.import('../app/testimony/testimony_model'),
-    TestimonyPicture: sequelize.import('../app/testimony/testimony_picture_model'),
+    AppToken: sequelize.import('../app/app_token'),
+    AppUser: sequelize.import('../app/app_user'),
+    DashboardToken: sequelize.import('../app/dashboard_token'),
+    DashboardUser: sequelize.import('../app/dashboard_user'),
+    Report: sequelize.import('../app/report'),
+    Role: sequelize.import('../app/dashboard_user')
 }
 
 Object.keys(models).forEach((modelName) => {
