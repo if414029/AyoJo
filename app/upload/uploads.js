db4474cdbda869a2c6f1729a501f1d3adbf79ebc
Amos Sitorus <amos@qlue.id>
2019-01-12 12:53:40 +0700

    change function upload

--- app/upload/uploads.js
+++ app/upload/uploads.js
@@ -8,29 +8,21 @@ const multer = Multer({
   storage: Multer.MemoryStorage,
 })
 
-router.post('/multi', multer.array('file', 10), imgUpload.uploadMulti, (req, res, next) => {
-    res.status(200).json({
-      status: true,
-      message: 'OK',
-      result: req.fileArr,
-    })
+router.post('/multi',multer.array('file',10), imgUpload.uploadMulti, function(req, res, next) {
+  res.status(200).json({
+    status: true,
+    message: 'OK',
+    result: req.fileArr
+  })
 })
 
-router.post('/single', multer.single('file'), imgUpload.uploadSingle, (req, res, next) => {
-  if(req.file && req.file.cloudStoragePublicUrl){
-    res.status(200).json({
-      status: true,
-      message: 'OK',
-      result: req.filePhoto,
-    })
-  } else {
-    res.status(500).json({
-      status: false,
-      message: 'An error has occured, please try again.',
-      result: [],
-    })
-  }
+router.post('/single',multer.single('file'), imgUpload.uploadSingle, function(req, res, next) {
+  res.status(200).json({
+    status: true,
+    message: 'OK',
+    result: req.fileSingle
+  })
 })
 
 
-module.exports = router
+module.exports = router;
